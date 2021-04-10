debe9d00219adaad2b881b33637b59dbe03a5dec
aparnamicheal <aparnamicheal97@gmail.com>
2021-04-10 19:09:38 +0530

    second commit

--- src/server.js
+++ src/server.js
@@ -11,5 +11,5 @@ var express = require('express');
  var server = app.listen(port, function () {
     var host = server.address().address
     
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening started http://%s:%s", host, port)
  })
